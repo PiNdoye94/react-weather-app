424b593266019ebfb859e1069cbfb15a490aeaae
PiNdoye94 <PiNdoye94>
2021-11-06 20:08:59 +0000

    update footer

--- src/App.js
+++ src/App.js
@@ -6,6 +6,9 @@ function App() {
   return (
     <div className="App">
       <Weather />
+      <footer className="footer">
+        By <a href="http://Teknuflex.tech">TeknuFlex</a> Todo App with Reactjs
+      </footer>
     </div>
   );
 }
